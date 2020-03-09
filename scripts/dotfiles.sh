679818419c3cc7d66c32341b008bf66c8a8b3567
Chandler Swift <chandler@chandlerswift.com>
2020-03-09 16:40:18 -0500

    Update packer config

--- scripts/dotfiles.sh
+++ scripts/dotfiles.sh
@@ -2,7 +2,9 @@
 
 echo "Installing dotfiles..."
 
-# TODO  git clone https://github.com/chandlerswift/dotfiles.git $HOME/dotfiles
-# TODO
-# TODO  cd $HOME/dotfiles
-# TODO  ./install.sh
+cd $HOME
+git init
+git remote add origin https://github.com/ChandlerSwift/dotfiles.git
+git fetch
+git checkout -f master
+git remote set-url origin git@github.com:ChandlerSwift/dotfiles.git
